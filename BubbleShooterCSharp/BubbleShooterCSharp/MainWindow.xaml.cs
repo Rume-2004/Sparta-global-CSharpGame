76a62144ec721082b69798a4f77a71ace7cb6312
Rume Rhuada <rumerhuada@spartaglobal.com>
2019-02-25 15:18:46 +0000

    progreesing in the game

---
+++ BubbleShooterCSharp/BubbleShooterCSharp/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BubbleShooterCSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            for (int i = 0; i < 5; i++)
+            {
+
+            }
+        }
+        //private static Image CreateImage()
+        //{
+        //    Image ball = new Image();
+        //    ball.Width = 50;
+        //    ball.Height = 50;
+        //    ImageSource ballImage = new BitmapImage(new Uri(ImgNameball));
+        //    ball.Source = ballImage;
+        //    return ball;
+        //}
+        //public class Bubbles
+        //{
+        //    imageFile.Source = new BitmapImage(new Uri(ofd.FileName));
+
+
+        //}
+        //public void mapArray1()
+        //{
+        //    for (var y = 0; y < ?.Length; y++)
+        //    {
+        //        for (var x = 0; x < ?[y].Length; x++)
+        //        {
+
+        //        }
+        //    }
+        //}
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Instructions_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton44_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton48_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton47_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton46_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton45_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton43_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton42_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton41_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton40_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Imagebutton39_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    
+}
