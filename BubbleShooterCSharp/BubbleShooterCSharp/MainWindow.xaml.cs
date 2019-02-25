3641ab55b9887bf56044d366906919416b63c626
Rume Rhuada <rumerhuada@spartaglobal.com>
2019-02-25 17:57:17 +0000

    I have made the click functions for each button

--- BubbleShooterCSharp/BubbleShooterCSharp/MainWindow.xaml.cs
+++ BubbleShooterCSharp/BubbleShooterCSharp/MainWindow.xaml.cs
@@ -37,246 +37,268 @@ namespace BubbleShooterCSharp
 
         private void Imagebutton_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton1_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton1.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton44_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton44.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton48_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton48.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton47_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton47.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton46_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton46.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton45_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton45.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton2_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton2.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton4_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton4.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton43_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton43.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton42_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton42.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton41_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton41.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton40_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton40.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton39_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton39.Visibility = Visibility.Hidden;
         }
-
         private void Imagebutton36_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton36.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton6_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton6.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton14_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton14.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton28_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton28.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton29_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton29.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton15_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton15.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton27_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton27.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton30_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton30.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton37_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton37.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton5_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton5.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton17_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton17.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton26_Click(object sender, RoutedEventArgs e)
         {
-
+            imagebutton26.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton31_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton31.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton38_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton38.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton3_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton3.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton8_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton8.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton18_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton18.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton25_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton25.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton32_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton32.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton9_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton9.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton7_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton7.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton19_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton19.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton24_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton24.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton33_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton33.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton10_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton10.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton12_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton12.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton20_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton20.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton23_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton23.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton34_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton34.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton11_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton11.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton13_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton13.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton21_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton21.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton22_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton22.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton35_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton35.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton49_Click(object sender, RoutedEventArgs e)
         {
+            imagebutton49.Visibility = Visibility.Hidden;
 
         }
     }
