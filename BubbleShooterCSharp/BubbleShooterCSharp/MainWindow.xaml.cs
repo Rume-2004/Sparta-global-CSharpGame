25f3d5b1296c1bc3c24ecf074c506c4a41aa6b6f
Rume Rhuada <rumerhuada@spartaglobal.com>
2019-02-26 10:23:54 +0000

    I have removed just the image source when clicked and not the entire button

--- BubbleShooterCSharp/BubbleShooterCSharp/MainWindow.xaml.cs
+++ BubbleShooterCSharp/BubbleShooterCSharp/MainWindow.xaml.cs
@@ -37,268 +37,268 @@ namespace BubbleShooterCSharp
 
         private void Imagebutton_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton.Visibility = Visibility.Hidden;
+            ball.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton1_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton1.Visibility = Visibility.Hidden;
+            ball1.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton44_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton44.Visibility = Visibility.Hidden;
+            ball44.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton48_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton48.Visibility = Visibility.Hidden;
+            ball48.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton47_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton47.Visibility = Visibility.Hidden;
+            ball47.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton46_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton46.Visibility = Visibility.Hidden;
+            ball46.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton45_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton45.Visibility = Visibility.Hidden;
+            ball45.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton2_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton2.Visibility = Visibility.Hidden;
+            ball2.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton4_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton4.Visibility = Visibility.Hidden;
+            ball4.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton43_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton43.Visibility = Visibility.Hidden;
+            ball43.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton42_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton42.Visibility = Visibility.Hidden;
+            ball42.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton41_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton41.Visibility = Visibility.Hidden;
+            ball41.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton40_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton40.Visibility = Visibility.Hidden;
+            ball40.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton39_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton39.Visibility = Visibility.Hidden;
+            ball39.Visibility = Visibility.Hidden;
         }
         private void Imagebutton36_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton36.Visibility = Visibility.Hidden;
+            ball36.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton6_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton6.Visibility = Visibility.Hidden;
+            ball6.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton14_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton14.Visibility = Visibility.Hidden;
+            ball14.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton28_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton28.Visibility = Visibility.Hidden;
+            ball28.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton29_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton29.Visibility = Visibility.Hidden;
+            ball29.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton15_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton15.Visibility = Visibility.Hidden;
+            ball15.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton27_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton27.Visibility = Visibility.Hidden;
+            ball27.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton30_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton30.Visibility = Visibility.Hidden;
+            ball30.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton37_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton37.Visibility = Visibility.Hidden;
+            ball37.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton5_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton5.Visibility = Visibility.Hidden;
+            ball5.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton17_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton17.Visibility = Visibility.Hidden;
+            ball17.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton26_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton26.Visibility = Visibility.Hidden;
+            ball26.Visibility = Visibility.Hidden;
         }
 
         private void Imagebutton31_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton31.Visibility = Visibility.Hidden;
+            ball31.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton38_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton38.Visibility = Visibility.Hidden;
+            ball38.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton3_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton3.Visibility = Visibility.Hidden;
+            ball3.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton8_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton8.Visibility = Visibility.Hidden;
+            ball8.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton18_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton18.Visibility = Visibility.Hidden;
+            ball18.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton25_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton25.Visibility = Visibility.Hidden;
+            ball.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton32_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton32.Visibility = Visibility.Hidden;
+            ball32.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton9_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton9.Visibility = Visibility.Hidden;
+            ball9.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton7_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton7.Visibility = Visibility.Hidden;
+            ball7.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton19_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton19.Visibility = Visibility.Hidden;
+            ball19.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton24_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton24.Visibility = Visibility.Hidden;
+            ball24.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton33_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton33.Visibility = Visibility.Hidden;
+            ball33.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton10_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton10.Visibility = Visibility.Hidden;
+            ball10.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton12_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton12.Visibility = Visibility.Hidden;
+            ball12.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton20_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton20.Visibility = Visibility.Hidden;
+            ball20.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton23_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton23.Visibility = Visibility.Hidden;
+            ball23.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton34_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton34.Visibility = Visibility.Hidden;
+            ball34.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton11_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton11.Visibility = Visibility.Hidden;
+            ball11.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton13_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton13.Visibility = Visibility.Hidden;
+            ball13.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton21_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton21.Visibility = Visibility.Hidden;
+            ball21.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton22_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton22.Visibility = Visibility.Hidden;
+            ball22.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton35_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton35.Visibility = Visibility.Hidden;
+            ball35.Visibility = Visibility.Hidden;
 
         }
 
         private void Imagebutton49_Click(object sender, RoutedEventArgs e)
         {
-            imagebutton49.Visibility = Visibility.Hidden;
+            ball49.Visibility = Visibility.Hidden;
 
         }
     }
